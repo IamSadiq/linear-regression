48174f13855a8d421dca195977eb52df84c7c7a6
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-04 17:32:17 +0100

    modified generate-date

--- generate-data.py
+++ generate-data.py
@@ -2,37 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-with open('data/sample.csv', 'wb') as csv_file:
-    fileWriter = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    fileWriter.writerow(['Hey', 'Hello'])
+N = 70
+X = np.linspace(-1, 1, N)
+Y = 3 * X + np.random.randn(N)
 
-X = []
-Y = []
+# with open('data/sample.csv', 'wb') as csv_file:
+#     fileWriter = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#     fileWriter.writerow([X, Y])
 
-# print('X\tY')
-# file = open('data/sample.csv').read()
+# for line in open('data/sample.csv'):
+#     a, b = line.split(',')
+#     print(a, b)
 
-# for x in range(50):
-#     y = 2*x+3
 
-#     # file.writable = True
-#     # file.write(x)
-#     # file.write(',')
-#     # file.write(y)
-
-#     X.append(float(x))
-#     Y.append(float(y))
-
-#     print(x, '\t', 2*x+3)
-
-# # for line in open('data/sample.csv'):
-# #     print(line)
+# file.writable = True
+# file.write(x)
 
 # X = np.array(X)
 # Y = np.array(Y)
 
-# plt.scatter(X, Y)
-# # plt.plot(Y)
-# plt.show()
+plt.scatter(X, Y)
+# plt.plot(Y)
+plt.show()
 
 # file.close()
